f3b6eba4daa0a78ad9bd4df7ebff3caf612fa050
Mark <markalfredvd@gmail.com>
2023-07-08 08:47:22 +0200

    Add firstLast6 exercise

--- Array-1/firstLast6/src/index.ts
+++ Array-1/firstLast6/src/index.ts
@@ -1,8 +1,6 @@
-
-
 export function firstLast6(numbers: number[]): boolean {
-    if ((numbers[0] == 6) || (numbers[numbers.length -1] == 6)) {
-        return true
-    }
-    return false
+  if (numbers[0] == 6 || numbers[numbers.length - 1] == 6) {
+    return true;
+  }
+  return false;
 }
