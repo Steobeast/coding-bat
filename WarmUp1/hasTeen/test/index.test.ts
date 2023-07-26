83d8e4402157bdaf3f3ba376cab9e6057321c167
Mark <markalfredvd@gmail.com>
2023-07-26 20:18:26 +0200

    Add has teen exercise

---
+++ WarmUp1/hasTeen/test/index.test.ts
@@ -0,0 +1,31 @@
+/*
+
+We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+Given 3 int values, return true if 1 or more of them are teen.
+
+
+hasTeen(13, 20, 10) → true
+hasTeen(20, 19, 10) → true
+hasTeen(20, 10, 13) → true
+*/
+
+import { assert } from "chai";
+import { hasTeen } from "../src";
+
+
+describe("hasTeen", () => {
+    it("Should return true if 1 of the values is within range", () => {
+        const result = hasTeen(13, 20, 10);
+        assert.equal(result, true)
+    })
+    it("Should return true if 2 of the values are within range", () => {
+        const result = hasTeen(13, 14, 10);
+        assert.equal(result, true)
+    })
+
+    it("Should return false if no values are within range", () => {
+        const result = hasTeen(12, 20, 10);
+        assert.equal(result, false)
+    })
+
+})
