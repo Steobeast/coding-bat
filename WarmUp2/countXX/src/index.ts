e3927787ece776cf727ebeb5b0153e31e412830c
Mark <markalfredvd@gmail.com>
2023-07-26 20:56:30 +0200

    Removed gitignore file

--- WarmUp2/countXX/src/index.ts
+++ WarmUp2/countXX/src/index.ts
@@ -1,11 +1,9 @@
-
-
 export function countXX(str: string): number {
-    let count = 0
-    for (let index = 0; index < str.length; index++) {
-        if (str.substring(index, index+2) == ('xx')) {
-            count = count + 1   
-        }
+  let count = 0;
+  for (let index = 0; index < str.length; index++) {
+    if (str.substring(index, index + 2) == "xx") {
+      count = count + 1;
     }
-    return count
+  }
+  return count;
 }
