4bfd598227d4fa277ca6462ca0d9fa1f14a97439
Mark <markalfredvd@gmail.com>
2023-08-04 11:34:38 +0200

    added new tests for when neither are teens

--- WarmUp1/loneTeen/test/index.test.ts
+++ WarmUp1/loneTeen/test/index.test.ts
@@ -12,18 +12,29 @@ loneTeen(13, 13) → false
 import { assert } from "chai";
 import { loneTeen } from "../src";
 
-
 describe("loneTeen", () => {
-    it("Should return true if only one number falls within range", () => {
-        const result = loneTeen(13, 99);
-        assert.equal(result, true)
-    })
-    it("Should return true if only the other number falls within range", () => {
-        const result = loneTeen(99, 13);
-        assert.equal(result, true)
-    })
-    it("Should return false if both numbers fall within range", () => {
-        const result = loneTeen(13, 13);
-        assert.equal(result, false)
-    })
-}) 
+  it("Should return true if only one number falls within range", () => {
+    const result = loneTeen(13, 99);
+    assert.equal(result, true);
+  });
+
+  it("Should return true if only the other number falls within range", () => {
+    const result = loneTeen(99, 13);
+    assert.equal(result, true);
+  });
+
+  it("Should return false if both numbers fall within range", () => {
+    const result = loneTeen(13, 13);
+    assert.equal(result, false);
+  });
+  
+  it("Should return false if both numbers are out of range", () => {
+    const result = loneTeen(27, 8);
+    assert.equal(result, false);
+  });
+
+  it("Should return false if a negative number is given", () => {
+    const result = loneTeen(-19, 0);
+    assert.equal(result, false);
+  });
+});
