429e184beae23e999aa879f63dd83b9f5b5198b6
Mark <markalfredvd@gmail.com>
2023-08-04 11:25:03 +0200

    Add  more tests to lastDigit exercise

--- WarmUp1/lastDigit/test/index.test.ts
+++ WarmUp1/lastDigit/test/index.test.ts
@@ -8,20 +8,33 @@ lastDigit(6, 17) → false
 lastDigit(3, 113) → true
 */
 
-import { assert } from "chai"
-import { lastDigit } from "../src"
+import { assert } from "chai";
+import { lastDigit } from "../src";
 
 describe("lastDigit()", () => {
-    it("Should return true if the last digit in each int are the same", () => {
-        const result = lastDigit(7, 17)
-        assert.equal(result, true)
-    })
-    it("Should return false if the last digit in each int are differnt", () => {
-        const result = lastDigit(6, 17)
-        assert.deepEqual(result, false)
-    })
-    it("Should return true if the last digit in each int are the same", () => {
-        const result = lastDigit(3, 113)
-        assert.equal(result, true)
-    })
-})
+
+  it("Should return true if the last digit in each int are the same", () => {
+    const result = lastDigit(7, 17);
+    assert.equal(result, true);
+  });
+
+  it("Should return false if the last digit in each int are differnt", () => {
+    const result = lastDigit(6, 17);
+    assert.deepEqual(result, false);
+  });
+  
+  it("Should return true if the last digit in each int are the same", () => {
+    const result = lastDigit(3, 113);
+    assert.equal(result, true);
+  });
+    
+  it("Should return true if the last digit in each int are the same with two values in the 100's", () => {
+    const result = lastDigit(263, 113);
+    assert.equal(result, true);
+  });
+  
+  it("Should return true if the last digit in each are the same with 2 values in the 1000's", () => {
+    const result = lastDigit(785463, 253513);
+    assert.equal(result, true);
+  });
+});
